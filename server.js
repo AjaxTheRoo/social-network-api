df3fc88980f966469c8aa6641c5a7e7bc016ce43
AjaxTheRoo <rivetwolf@gmail.com>
2025-03-20 01:00:47 -0700

    upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Use routes
+app.use('/users', userRoutes);
+app.use('/thoughts', thoughtRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
